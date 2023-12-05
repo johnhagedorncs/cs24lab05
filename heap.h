3ac2555172b394d8ff668f458e7f8015bfa951a2
johnhagedorncs <johnhagedorn@ucsb.edu>
2023-12-04 19:13:18 -0800

    bruh

--- heap.h
+++ heap.h
@@ -1,23 +1,25 @@
 // heap.h
 // Binary heap tree header file for CS 24 lab
 // Diba Mirza
-
 #ifndef HEAP_H
 #define HEAP_H
-
 #include <iostream>
 #include <vector>
+using namespace std;
 
 class Heap{
 
  public:
-    void push(int value);     // insert value; //duplicates are allowed
-    void pop(); // delete the min element 
-    int top();
-    bool empty();
+   void push(int value); // insert value; //duplicates are allowed
+   void pop(); // delete the min element 
+   int top() const;
+   bool empty() const;
+
+   void heapifyUp(int index);
+   void heapifyDown(int index);
     
  private:
-    std::vector<int> vdata; //store the binary heap tree as a dynamic array
+   vector<int> vdata; //store the binary heap tree as a dynamic array
 };
 
 #endif
